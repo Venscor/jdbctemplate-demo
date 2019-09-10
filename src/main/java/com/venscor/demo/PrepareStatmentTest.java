2a8dd2f0c32b4a198a5115137a1a863daa94a475
venscor <venscor@163.com>
2019-09-10 15:28:52 +0800

    mybtis like

--- src/main/java/com/venscor/demo/PrepareStatmentTest.java
+++ src/main/java/com/venscor/demo/PrepareStatmentTest.java
@@ -1,4 +1,29 @@
 package com.venscor.demo;
 
+import java.sql.*;
+
 public class PrepareStatmentTest {
+
+    public static void main(String[] args) {
+        Connection con= null;
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/demo", "root", "nipc1404");
+            PreparedStatement pstmt=con.prepareStatement("select  name from school where  name like ?");
+
+            pstmt.setString(1,"%xd");
+
+            ResultSet set = pstmt.executeQuery();
+
+            while (set.next()){
+                String a = set.getString(1);
+
+                System.out.printf("");
+
+            }
+            System.out.printf("");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
